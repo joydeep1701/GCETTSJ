34fdc15ad894b0836e48227fd1a25dddb2316b82
Joydeep Mukherjee <joydeep1701@gmail.com>
2018-02-06 10:53:47 +0530

    Rebuild Start

--- temp/1512899905.6263971.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(void) {
-    int m,n;
-    scanf("%d %d",&m,&n);
-    printf("%d",m/n);
-}
--- temp/1512900020.2794423.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(void) {
-    int m,n;
-    scanf("%d %d",&m,&n);
-    printf("%d",m/n);
-    for(int i=0;;i++);
-}
