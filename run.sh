d6912bf0ad59e175fa56c600a7fea9890a8b6968
Joydeep Mukherjee <joydeep1701@gmail.com>
2018-02-09 17:48:58 +0530

    Old Code Removed

--- run.sh
+++
@@ -1,8 +0,0 @@
-cd ..
-. env/bin/activate
-cd GCETTSJ
-export FLASK_APP=hello.py
-export FLASK_DEBUG=1
-flask run
-
-
--- wsgi.py
+++
@@ -1,5 +0,0 @@
-from hello import app as application
-
-if __name__=='__main__':
-	application.run(host='0.0.0.0')
-
